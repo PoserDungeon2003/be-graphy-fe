d7b0f250428fb9da5c41f32c6d115b929da597eb
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2025-02-16 14:10:36 +0700

    Add signup page

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import {
 import { useState } from "react";
 import Home from "./pages/home";
 import Login from "./pages/login";
+import Signup from "./pages/signup";
 
 function App() {
   const [queryClient] = useState(
@@ -44,6 +45,7 @@ function App() {
           >
             <Route index element={<Home />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
           </Route>
         </Routes>
       </BrowserRouter>
