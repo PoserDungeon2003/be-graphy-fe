3a554a5268ef80f1893b89fdbfca840ec1d08119
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2025-02-16 20:52:13 +0700

    Add PhotoPost component

--- src/pages/signup.verify.tsx
+++ src/pages/signup.verify.tsx
@@ -6,7 +6,10 @@ import { Link } from "react-router";
 export default function SignupVerify() {
   const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    index: number,
+  ) => {
     const value = e.target.value;
     if (/^\d*$/.test(value)) {
       e.target.value = value;
@@ -32,7 +35,7 @@ export default function SignupVerify() {
   };
 
   return (
-    <div className="relative flex h-full w-full flex-col items-center justify-center gap-8 xl:gap-12 overflow-y-hidden">
+    <div className="relative flex h-full w-full flex-col items-center justify-center gap-8 overflow-y-hidden xl:gap-12">
       <div className="absolute top-0 left-0 mt-4 ml-4">
         <Link
           to={"/"}
@@ -45,26 +48,30 @@ export default function SignupVerify() {
       <img
         src="/images/effect/gradient_green_r.png"
         alt="effect"
-        className="absolute bottom-0 left-0 rotate-180 -z-10 w-1/2"
+        className="absolute bottom-0 left-0 -z-10 w-1/2 rotate-180"
       />
-      <div className="relative w-1/2 flex flex-col items-center justify-center">
+      <div className="relative flex w-1/2 flex-col items-center justify-center">
         <img
           src="/images/button/btn_gradient_blue_lg.png"
           alt="effect"
           className="absolute -z-10"
         />
-        <img src="/images/icons/camera.png" alt="camera" className="absolute w-1/4 -left-5 -top-20" />
-        <h1 className="relative text-center text-4xl font-bold text-black z-10">
+        <img
+          src="/images/icons/camera.png"
+          alt="camera"
+          className="absolute -top-20 -left-5 w-1/4"
+        />
+        <h1 className="relative z-10 text-center text-4xl font-bold text-black">
           Account Verification
         </h1>
       </div>
-      <span className="text-black text-lg">
+      <span className="text-lg text-black">
         Please enter the 4 digit code sent to your email
       </span>
       <form className="flex flex-col items-center gap-4">
         <div className="flex items-center justify-between gap-8">
           {_.map([0, 1, 2, 3], (index) => (
-            <div key={index} className="relative w-32 h-16">
+            <div key={index} className="relative h-16 w-32">
               <img
                 src="/images/background/bg_otp_input.png"
                 alt="otp_input"
@@ -75,7 +82,7 @@ export default function SignupVerify() {
                   inputRefs.current[index] = el;
                 }}
                 type="text"
-                className="absolute inset-0 w-full h-full bg-transparent text-black font-bold text-center text-2xl px-3 border-0 focus:outline-none focus:ring-0 focus:shadow-none"
+                className="absolute inset-0 h-full w-full border-0 bg-transparent px-3 text-center text-2xl font-bold text-black focus:ring-0 focus:shadow-none focus:outline-none"
                 maxLength={1}
                 pattern="\d*"
                 onChange={(e) => handleInputChange(e, index)}
@@ -84,13 +91,16 @@ export default function SignupVerify() {
             </div>
           ))}
         </div>
-        <a href="" className="text-black underline uppercase font-bold mt-2">
+        <a href="" className="mt-2 font-bold text-black uppercase underline">
           Resend code
         </a>
-        <button type="submit" className="text-black text-2xl border-2 border-black px-8 py-6 rounded-lg uppercase font-bold">
+        <button
+          type="submit"
+          className="rounded-lg border-2 border-black px-8 py-6 text-2xl font-bold text-black uppercase"
+        >
           Verify
         </button>
       </form>
     </div>
-  )
+  );
 }
